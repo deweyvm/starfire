de114283319a7e9a9ca1ee51fd265c84ae50b79c
deweyvm <deweyvm@gmail.com>
2014-01-26 16:11:43 -0500

    fixed reader bug

--- src/com/deweyvm/dogue/starfire/Starfire.scala
+++ src/com/deweyvm/dogue/starfire/Starfire.scala
@@ -14,23 +14,19 @@ class Starfire extends Task {
   override def execute() {
     Log.info("Starting server")
     val server = new ServerSocket(port)
-    server.setSoTimeout(1000)
     Log.info("Server started successfully")
     while(running && !server.isClosed) {
-      try {
-        val connection = server.accept()
-        currentReader foreach {
-          Log.info("Killing old reader")
-          _.kill()
-        }
-        Log.info("Spawning reader")
-        val reader = new StarReader(connection, this)
-        reader.start()
-        currentReader = reader.some
-      } catch {
-        case ste:SocketTimeoutException =>
-          Thread.sleep(100)
+      Log.info("Awaiting connections")
+      val connection = server.accept()
+      currentReader foreach {
+        Log.info("Killing old reader")
+        _.kill()
       }
+      Log.info("Spawning reader")
+      val reader = new StarReader(connection, this)
+      reader.start()
+      currentReader = reader.some
+
     }
     Log.info("Shutting down")
   }
